6a5854d805f144ce993670e87f1379a3f05dc4c3
Mohanad Elhag <moh.7afiz@gmail.com>
2017-08-02 02:37:08 +0300

    Add-client.component done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+// AngularFire Imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+// Components Import
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { RegisterComponent } from './components/register/register.component';
+// Service Imports
+import { ClientService } from './services/client.service';
+// Router Configuration
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'add-client', component: AddClientComponent}
+];
+// Firebase Configuration
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAWuiFVp7Cm-NhksrxcRAwV3ZX4h0NxX2Q',
+    authDomain: 'https://client-panel-15a04.firebaseio.com/',
+    databaseURL: 'https://client-panel-15a04.firebaseio.com',
+    storageBucket: 'client-panel-15a04.appspot.com',
+    messagingSenderId: '109104448244'
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddClientComponent,
+    ClientDetailsComponent,
+    ClientsComponent,
+    DashboardComponent,
+    EditClientComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    SidebarComponent,
+    SettingsComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
